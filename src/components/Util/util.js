9eefa8f2c4befb12945576dc47bb86e0d3eb06fd
Meharetech <meharetech420@gmail.com>
2024-11-07 17:18:32 +0530

    first commit

--- src/components/Util/util.js
+++ src/components/Util/util.js
@@ -1,10 +1,11 @@
 // export const BaseUrl = 'https://back.moneymindertradingcompany.co.in';
 
 
-// export const BaseUrl = 'http://88.222.215.234:7000';
+// export const BaseUrl = 'https://88.222.215.234:7000';
+export const BaseUrl = 'https://bloodbackoffice.onrender.com';
 
 
-export const BaseUrl = 'http://localhost:7000';
+// export const BaseUrl = 'http://localhost:7000';
 
 
-// export const BaseUrl = 'https://blooddonationbackend.vercel.app';
+// export const BaseUrl = 'https://blood-back-office.vercel.app';
