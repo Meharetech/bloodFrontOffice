6e0cf1ee0ff51c87cccc0cdb8aa6dc9ad3ef4b1f
DeepakPoonia0000 <dpoonia0000@gmail.com>
2024-11-14 16:52:07 +0530

    nav bar

--- src/components/Util/util.js
+++ src/components/Util/util.js
@@ -2,8 +2,8 @@
 
 
 // export const BaseUrl = 'https://88.222.215.234:7000';
-// export const BaseUrl = 'https://bloodbackoffice.onrender.com';
-export const BaseUrl = 'http://localhost:7000';
+export const BaseUrl = 'https://bloodbackoffice.onrender.com';
+// export const BaseUrl = 'http://localhost:7000';
 
 
 
